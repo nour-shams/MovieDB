823416f8dcab1fe7b2b83c9ddcf52a9b53312f3c
Nour Shams <nour0shams@outlook.com>
2020-10-21 22:20:57 +0300

    step4

--- index.js
+++ index.js
@@ -17,10 +17,37 @@ app.listen(port, () => {
 
 app.get('/test', (req, res) => res.send({status:200, message:"ok"}))
 
+app.get('/search',(req,res) => {
+    const search = req.query.s;
+
+    if (typeof search != 'undefined') {
+        // Search string applied
+        const response = {
+            status:200, message:"ok", data: search
+        };
+
+        res.send(response);
+    }
+    else {
+        const response = {
+            status:500, error:true, message: "you have to provide a search"
+        };
 
 
-  
-  app.get('/time', function (req, res) {
+        res.status(500);
+        res.send(response);
+    }
+});
+
+app.get('/time', function (req, res) {
     
     res.send({status:200, message: time});
   });
+
+
+      app.get('/hello/:ID', (req, res) => {
+        id = req.params;
+        res.send({ status: 200, message: "Hello, " + id.ID })
+    })
+
+    
